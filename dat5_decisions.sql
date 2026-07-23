aecbf83b0ad2db3483c387b56ca276be7c15bad8
Bhumireddy chandana <bhumireddyysirichandana@gmail.com>
2026-07-23 17:08:18 +0530

    day5_decisions.sql

---
+++ dat5_decisions.sql
@@ -0,0 +1,29 @@
+use supply_prescript;
+CREATE TABLE decisions (
+    decision_id INT AUTO_INCREMENT PRIMARY KEY,
+    prediction_id INT,
+    recommended_action VARCHAR(100),
+    selected_action VARCHAR(100),
+    decision_date DATE,
+    FOREIGN KEY (prediction_id) REFERENCES predictions(prediction_id)
+);
+INSERT INTO decisions
+(prediction_id,
+recommended_action,
+selected_action,
+decision_date)
+
+SELECT
+prediction_id,
+CASE
+    WHEN predicted_risk = 'High' THEN 'Expedite Shipment'
+    ELSE 'Normal Shipping'
+END,
+CASE
+    WHEN predicted_risk = 'High' THEN 'Expedite Shipment'
+    ELSE 'Normal Shipping'
+END,
+CURDATE()
+
+FROM predictions;
+select count(*) from decisions ;
